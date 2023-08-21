76214b2839ce929b8379bd68615301c9ffdc56d8
grzeg <grzegorzterenda@gmail.com>
2023-08-21 20:21:53 +0200

    Created Validation error service for nice error display :)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,24 @@
 import express = require("express");
-import {todoRouter} from "./routers/todo";
 import {pool} from "./utils/db";
+import 'express-async-errors';
+
+import {todoRouter} from "./routers/todo";
+import './utils/db';
+import {handleError, ValidationError} from "./utils/errors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/", todoRouter);
+app.use(handleError);
 
 app.get("/test", async (req, res) => {
     try {
         const [result] = await pool.execute("SELECT * FROM `todos`");
         res.json(result);
     } catch (err) {
-        console.log("Error:", err);
-        res.status(500).json({message: "Server error"});
+        throw new ValidationError("wrong connection");
     }
 })
 
