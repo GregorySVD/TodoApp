5fc96ad9fe2ff3923082a90729178e3b0a9c9faf
grzeg <grzegorzterenda@gmail.com>
2023-08-21 19:50:39 +0200

    Created pool to DB and tested it

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,22 @@
 import express = require("express");
 import {todoRouter} from "./routers/todo";
+import {pool} from "./utils/db";
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/', todoRouter);
+
+app.get('/test', async (req, res) => {
+    try {
+        const [result] = await pool.execute("SELECT * FROM `todos`");
+        res.json(result);
+    } catch (err) {
+        console.log("Error:", err);
+        res.status(500).json({message: "Server error"});
+    }
+})
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
--- backend/src/routers/todo.ts
+++ backend/src/routers/todo.ts
@@ -1,9 +1,9 @@
 import {Router} from "express";
 
-export const todoRouter = Router ();
+export const todoRouter = Router();
 
 
 todoRouter
     .get('/', async (req, res) => {
-        res.json({name: 'Jazda'});
+        res.json({name: 'TODO'});
     })
