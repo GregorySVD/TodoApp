6bb5d961b71695bb2ba06b84ef55a49b50c5692f
grzeg <grzegorzterenda@gmail.com>
2023-09-10 16:48:48 +0200

    Changed task list from ul to table display

--- frontend/src/components/TaskList/OneTask.tsx
+++ frontend/src/components/TaskList/OneTask.tsx
@@ -4,6 +4,7 @@ import {TaskContext} from "../../context/TaskContext";
 import {Loader} from "../common/Loader/Loader";
 import {FetchDataContext} from "../../context/FetchDataContext.tsx";
 import './OneTask.css';
+import {TaskTableRow} from "./TaskTableRow";
 
 
 export const OneTask = () => {
@@ -13,30 +14,8 @@ export const OneTask = () => {
     if (!contextTask || !contextFetch)
         return <Loader/>;
 
-    const {setFetchData} = contextFetch;
     const {tasks} = contextTask;
 
-    const switchIsDoneState = async (taskId: string | undefined) => {
-        try {
-            const res = await fetch(`http://localhost:3001/todo/switch/${taskId}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            })
-            setFetchData(true);
-            await res.json();
-        } catch (err) {
-            console.error('An error occurred while updating isDone status:', err);
-        }
-    }
-
-    const deleteTask = async (taskId: string | undefined) => {
-        await fetch(`http://localhost:3001/todo/${taskId}`, {
-            method: 'DELETE',
-        });
-        setFetchData(true);
-    }
 
     if (!tasks) {
         return <Loader/>
@@ -44,17 +23,21 @@ export const OneTask = () => {
 
     return (
         <div>
-            <ul className="OneTask__List">
+            <table>
+                <thead>
+                <tr>
+                    <th>Task title</th>
+                    <th>Task status</th>
+                    <th>Remove task</th>
+                </tr>
+                </thead>
+                <tbody>
                 {tasks.map((task) => (
-                    <li key={task.id} className={task.isDone ? 'done' : 'undone'}>
-                        {task.title}
-                        {task.isDone == false ? <Btn text="✅ Done" onClick={() => switchIsDoneState(task.id)}/> :
-                            <Btn text="⛔ Undone" onClick={() => switchIsDoneState(task.id)}/>}
-                        <Btn text="🗑️" onClick={() => deleteTask(task.id)}/>
-                        {/* eslint-disable-next-line react/jsx-no-undef */}
-                    </li>
+                    <TaskTableRow key={task.id} task={task}/>
                 ))}
-            </ul>
+                </tbody>
+            </table>
+
         </div>
     );
 
