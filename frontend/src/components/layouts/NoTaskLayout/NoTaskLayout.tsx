0722633d4833fd7a99b53425245e721005b41b0c
grzeg <grzegorzterenda@gmail.com>
2023-10-01 20:27:17 +0200

    Refactor code for no task layout

--- frontend/src/components/layouts/NoTaskLayout/NoTaskLayout.tsx
+++ frontend/src/components/layouts/NoTaskLayout/NoTaskLayout.tsx
@@ -1,15 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './NoTaskLayout.css'
-import {useOpenAddTaskFormContext} from "../../../context/OpenAddTaskFormContext";
 import {AddTaskForm} from "../../AddTaskForm/AddTaskForm";
 
 export const NoTaskLayout = () => {
 
-    const {addTaskFormIsOpen, setAddTaskFormIsOpen} = useOpenAddTaskFormContext();
-
-    useEffect(() => {
-        setAddTaskFormIsOpen(false);
-    },[addTaskFormIsOpen, setAddTaskFormIsOpen]);
 
     return (
         <div className="NoTaskLayout__quote_container">
