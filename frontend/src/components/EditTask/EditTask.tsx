a289e658288de57ad3291a4bb69be13fd535f4cb
grzeg <grzegorzterenda@gmail.com>
2023-10-20 20:46:26 +0200

    Created modal for task editor

---
+++ frontend/src/components/EditTask/EditTask.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    onClick: () => Promise<void>;
+}
+export const EditTask = (props: Props) => {
+
+    return  (<>
+    <button className="EditTask__btn" onClick={() => props.onClick()}>
+
+        <i className="fa fa-edit"></i>
+    </button>
+
+    </>);
+}
--- frontend/src/components/TasksList/SingleTaskRow/SingleTaskRow.tsx
+++ frontend/src/components/TasksList/SingleTaskRow/SingleTaskRow.tsx
@@ -7,6 +7,7 @@ import {useTaskListRerenderContext} from "../../../context/TaskListRerenderConte
 import {useErrorContext} from "../../../context/ErrorContext";
 import {ErrorPage} from "../../layouts/ErrorPage/ErrorPage";
 import {toast} from "sonner";
+import {EditTask} from "../../EditTask/EditTask";
 
 interface Props {
     task: TodoEntity;
@@ -16,11 +17,32 @@ export const SingleTaskRow = (props: Props) => {
     const {setShouldRerender} = useTaskListRerenderContext();
     const {error, setError} = useErrorContext();
     const [modal, setModal] = useState<boolean>(false);
+    const [modalTaskEditor, setModalTaskEditor] = useState<boolean>(false);
     if (modal) {
         document.body.classList.add('active-modal');
     } else {
         document.body.classList.remove('active-modal');
     }
+
+    //@TODO: create method to update status of task
+    // const updateTask = async (taskId: string | undefined) => {
+    //     try {
+    //         const res = await fetch(
+    //             `http://localhost:3001/todo/${taskId}`, {
+    //                 method: 'GET',
+    //             }
+    //         )
+    //         if (!res.ok) {
+    //             await setError(new Error(`An error occurred while searching for this task. Try again later.`))
+    //             await toast.error('Error while searching for this tasks');
+    //         } else {
+    //             console.log(res);
+    //         }
+    //     } catch (err) {
+    //         await setError(new Error(`An error occurred while searching for this task. Try again later.`))
+    //         await toast.error('Error while searching for this tasks');
+    //     }
+    // }
 
     const switchDoneStatus = async (taskId: string | undefined) => {
         try {
@@ -46,13 +68,15 @@ export const SingleTaskRow = (props: Props) => {
     const toggleModal = async () => {
         await setModal(!modal);
     }
+    const toggleModalTaskEditor = async () => {
+        await setModalTaskEditor(!modalTaskEditor);
+    }
 
     const handleDeleteTask = async (taskId: string | undefined) => {
         try {
             const res = await fetch(`http://localhost:3001/todo/${taskId}`, {
                 method: 'DELETE',
             });
-
             if (!res.ok) {
                 await setError(new Error(`There was an error deleting ${taskId} task. Try again later.`));
                 await toast.error('Error while deleting task :(');
@@ -79,13 +103,36 @@ export const SingleTaskRow = (props: Props) => {
                             status={Boolean(props.task.isDone)}
                             onChange={() => switchDoneStatus(props.task.id)}
                         />
-                        <OneTaskRemoval
-                            onClick={() =>
-                                toggleModal()}
-                        />
+                        <OneTaskRemoval onClick={() => toggleModal()}/>
+                        <EditTask onClick={() => toggleModalTaskEditor()}/>
                     </div>
                 </div>
             </li>
+            {modalTaskEditor && (<div className="modal__confirmTaskRemove">
+                <div className="overlay"
+                     onClick={toggleModalTaskEditor}
+                >
+                </div>
+                <div className="modal-content">
+                    <button className="modal-close-btn" onClick={toggleModalTaskEditor}>
+                        <i className="fa fa-close"></i>
+                    </button>
+                    <h2>Are you sure you want to remove:
+                        <i>"{props.task.title}"</i> task ?
+                    </h2>
+                    <p>This action cannot be undone. 🗑️</p>
+                    <div className="modal-action-buttons">
+                        <button className="modal-accept-action--btn"
+                                onClick={async () => handleDeleteTask(props.task.id)}
+                        >Yes
+                        </button>
+                        <button className="modal-cancel-action-btn"
+                                onClick={toggleModal}
+                        >No
+                        </button>
+                    </div>
+                </div>
+            </div>)}
             {modal && (<div className="modal__confirmTaskRemove">
                 <div className="overlay"
                      onClick={toggleModal}
@@ -95,7 +142,9 @@ export const SingleTaskRow = (props: Props) => {
                     <button className="modal-close-btn" onClick={toggleModal}>
                         <i className="fa fa-close"></i>
                     </button>
-                    <h2>Are you sure you want to remove <i> {props.task.title} </i>task?</h2>
+                    <h2>Are you sure you want to remove:
+                        <i>"{props.task.title}"</i> task ?
+                    </h2>
                     <p>This action cannot be undone. 🗑️</p>
                     <div className="modal-action-buttons">
                         <button className="modal-accept-action--btn"
