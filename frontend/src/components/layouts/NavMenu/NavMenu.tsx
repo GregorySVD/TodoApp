068eade55fd63f9ea50d9671cafe149082b95dcb
grzeg <grzegorzterenda@gmail.com>
2023-10-31 21:06:54 +0100

    Implemented dark theme for change theme button

--- frontend/src/components/layouts/NavMenu/NavMenu.tsx
+++ frontend/src/components/layouts/NavMenu/NavMenu.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './NavMenu.css'
+import {useTheme} from "../../../context/ThemeContext";
 
 export const NavMenu = () => {
 
+    const {darkTheme, setDarkTheme} = useTheme();
 
+    const handleDarkThemeUse = () => {
+        setDarkTheme(!darkTheme);
+    }
+
+    const btnThemeChangedClass = darkTheme ? 'nav_btn-theme-changed dark-theme' : 'nav_btn-theme-changed';
 
     return <nav className="nav">
         <div className="nav_logo">
@@ -12,10 +19,11 @@ export const NavMenu = () => {
             <span className="nav_logo_text">TodoApp</span>
         </div>
         <div className="nav_btns">
-            <button className="nav_btn-theme-changed">
-                <span>DARK MODE</span>
-                <div className="fa fa-sun"></div>
-
+            <button className={btnThemeChangedClass}
+                    onClick={handleDarkThemeUse}
+            >
+                <span>{darkTheme ? 'LIGHT MODE' : 'DARK MODE'}</span>
+                <div className={`fa ${darkTheme ? 'fa-sun' : 'fa-moon'}`}></div>
             </button>
             <button className="nav_btn-contact">
                 <span>LET'S TALK</span>
