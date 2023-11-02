8534f7e12fe0f8bc3d2319bdf474faee1ea54194
grzeg <grzegorzterenda@gmail.com>
2023-11-02 19:11:50 +0100

    Fixed error with wrong div class returning when Light Theme was applied

--- frontend/src/components/layouts/NavMenu/NavBarContact/ContactContent/ContactContent.tsx
+++ frontend/src/components/layouts/NavMenu/NavBarContact/ContactContent/ContactContent.tsx
@@ -15,7 +15,7 @@ export const ContactContent = (props: Props) => {
                  onClick={props.onClick}
             >
             </div>
-            <div className={darkTheme ? "contact-content dark-theme" : "about-content"}>
+            <div className={darkTheme ? "contact-content dark-theme" : "contact-content"}>
                 <button className="contact-content-close-btn" onClick={props.onClick}>
                     <i className="fa fa-close"></i>
                 </button>
@@ -24,13 +24,17 @@ export const ContactContent = (props: Props) => {
                     <h1 className="contact-content-name">Grzegorz Terenda</h1>
                 </div>
                 <span>
-                    As a full-stack web developer, I am deeply passionate about crafting innovative and engaging web experiences. My journey into the world of technology began at a young age, and over the past year, I've dedicated myself to learning programming. The allure of this dynamic field has inspired me to pursue formal education in computer science. With a keen eye for continuous learning, I am excited to contribute my skills and enthusiasm to a forward-thinking team.
-                </span>
+                    I am a passionate software engineer with a strong interest in computer science. I have been fascinated by computers since I was a child, and I started learning to code a year ago. I am currently a student at MegaK, where I am learning to build web applications using Node.js, Express.js, TypeScript, MySQL, ReactJS and MongoDB. I am also a student at WSB University of Applied Sciences, where I am studying computer science.
+                      </span>
+                <span>
+I am always looking for new ways to improve my skills and knowledge. I am excited to see what the future holds for me in the field of software engineering.
+                    </span>
+
                 <span>Email: example@email.com</span>
                 <div className="contact-socials">
                     <div className="contact-socials-links">
                         <a href="https://www.facebook.com/grzegorz.terenda/" target="_blank"><i
-                            className="fa-brands fa-facebook" ></i></a>
+                            className="fa-brands fa-facebook"></i></a>
                         <a href="https://www.linkedin.com/in/grzegorz-terenda/" target="_blank"> <i
                             className="fa-brands fa-linkedin"></i></a>
                         <a href="https://github.com/GregorySVD" target="_blank"> <i
