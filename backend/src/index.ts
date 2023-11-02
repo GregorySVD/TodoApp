7897819aa8ed0f6a8179060d0fa4bacb220b97bb
grzeg <grzegorzterenda@gmail.com>
2023-11-02 19:53:08 +0100

    Implemented rate limiter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import express = require("express");
 import cors from "cors";
 import "express-async-errors";
+import {rateLimit} from "express-rate-limit";
 
 import {todoRouter} from "./routers/todo.ts";
 import {handleError} from "./utils/errors.ts";
 import {homeRouter} from "./routers/home.ts";
 
+
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+});
 app.use(cors({
     origin: "http://localhost:3000",
 }));
+app.use(limiter);
 
 app.use(express.json());
 
