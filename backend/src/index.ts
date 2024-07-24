ccb3539d3cf1da045fe58d8737c5c306368c5d00
GregorySVD <grzegorzterenda@gmail.com>
2024-07-24 18:08:20 +0200

    changed config vercel and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://todo-app-be-two.vercel.app/",
+    origin: "https://todoapp-sand.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
 
