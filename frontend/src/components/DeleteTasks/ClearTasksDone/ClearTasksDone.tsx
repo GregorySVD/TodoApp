f8cbc4fca4a8d8560b60e37312d74c261e4eb9a9
GregorySVD <grzegorzterenda@gmail.com>
2024-08-04 10:45:26 +0200

    FE: Updated fetch url to delete todos with is_done true status

--- frontend/src/components/DeleteTasks/ClearTasksDone/ClearTasksDone.tsx
+++ frontend/src/components/DeleteTasks/ClearTasksDone/ClearTasksDone.tsx
@@ -1,46 +1,49 @@
-import React, {useEffect} from 'react';
-import './ClearTasksDone.css'
-import {useTaskListRerenderContext} from "../../../context/TaskListRerenderContext";
-import {useErrorContext} from "../../../context/ErrorContext";
-import {ErrorPage} from "../../layouts/ErrorPage/ErrorPage";
-import {toast} from "sonner";
-import {useTheme} from "../../../context/ThemeContext";
-
+import React, { useEffect } from "react";
+import "./ClearTasksDone.css";
+import { useTaskListRerenderContext } from "../../../context/TaskListRerenderContext";
+import { useErrorContext } from "../../../context/ErrorContext";
+import { ErrorPage } from "../../layouts/ErrorPage/ErrorPage";
+import { toast } from "sonner";
+import { useTheme } from "../../../context/ThemeContext";
+import { BACKEND_URL_POSTGRES } from "src/utils/backend_URL";
 
 export const ClearTasksDone = () => {
+  const { darkTheme } = useTheme();
 
-    const {darkTheme} = useTheme();
+  const { setShouldRerender } = useTaskListRerenderContext();
+  const { error, setError } = useErrorContext();
 
-    const {setShouldRerender} = useTaskListRerenderContext();
-    const {error, setError} = useErrorContext();
-
-    useEffect(() => {
-        const appRoot = document.querySelector('.ClearTaskDone__btn');
-        if (appRoot) {
-            appRoot.classList.toggle('dark-theme', darkTheme);
-        }
-    }, [darkTheme]);
-
-    const handleClearTasksDone = async (): Promise<void> => {
-        try {
-            const res = await fetch(`http://localhost:3001/todo/done`, {
-                method: "DELETE",
-            });
-            if (!res.ok) {
-                await setError(new Error(`There was an error during clearing tasks done. Try again later.`));
-                await toast.error(`There was an error during clearing tasks done.`);
-            } else {
-                toast.success(`Clearing tasks done successfully! Keep it up!`);
-                setShouldRerender(true);
-            }
-        } catch (err) {
-            await setError(new Error(`There was an error during clearing tasks done. Try again later.`));
-            await toast.error(`There was an error during clearing tasks done.`)
-        }
+  useEffect(() => {
+    const appRoot = document.querySelector(".ClearTaskDone__btn");
+    if (appRoot) {
+      appRoot.classList.toggle("dark-theme", darkTheme);
     }
-    if (error) return <ErrorPage error={error}/>;
+  }, [darkTheme]);
 
-    return <div className="ClearTaskDone__container">
-        <button className="ClearTaskDone__btn" onClick={handleClearTasksDone}>Clear Tasks Done</button>
+  const handleClearTasksDone = async (): Promise<void> => {
+    try {
+      const res = await fetch(`${BACKEND_URL_POSTGRES}done`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        await setError(new Error(`There was an error during clearing tasks done. Try again later.`));
+        await toast.error(`There was an error during clearing tasks done.`);
+      } else {
+        toast.success(`Clearing tasks done successfully! Keep it up!`);
+        setShouldRerender(true);
+      }
+    } catch (err) {
+      await setError(new Error(`There was an error during clearing tasks done. Try again later.`));
+      await toast.error(`There was an error during clearing tasks done.`);
+    }
+  };
+  if (error) return <ErrorPage error={error} />;
+
+  return (
+    <div className="ClearTaskDone__container">
+      <button className="ClearTaskDone__btn" onClick={handleClearTasksDone}>
+        Clear Tasks Done
+      </button>
     </div>
-}
+  );
+};
