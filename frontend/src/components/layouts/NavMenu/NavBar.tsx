48af3abbdc109f5fdca35b0da5d1c59b123e81a8
GregorySVD <grzegorzterenda@gmail.com>
2024-08-04 17:01:51 +0200

    Updated about me content

--- frontend/src/components/layouts/NavMenu/NavBar.tsx
+++ frontend/src/components/layouts/NavMenu/NavBar.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import './NavBar.css'
-import {NavBarAboutOpenBtn} from "./NavBarAbout/NavBarAboutOpenBtn/NavBarAboutOpenBtn";
-import {NavBarThemChanger} from "./NavBarThemeChanger/NavBarThemChanger";
-import {NavBarContactOpener} from "./NavBarContact/NavBarContactOpener/NavBarContactOpener";
-import {Link} from "react-router-dom";
+import React from "react";
+import "./NavBar.css";
+import { NavBarAboutOpenBtn } from "./NavBarAbout/NavBarAboutOpenBtn/NavBarAboutOpenBtn";
+import { NavBarThemChanger } from "./NavBarThemeChanger/NavBarThemChanger";
+import { NavBarContactOpener } from "./NavBarContact/NavBarContactOpener/NavBarContactOpener";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
-
-    return <nav className="nav">
-        <div className="nav_logo">
-            <Link to="/" className="link-no-decoration" >
-
-            <span className="nav_logo_text">✅ TodoApp</span>
-            </Link>
-        </div>
-        <div className="nav_btns">
-            <NavBarThemChanger/>
-            <NavBarContactOpener/>
-            <NavBarAboutOpenBtn/>
-        </div>
+  return (
+    <nav className="nav">
+      <div className="nav_logo">
+        <Link to="/" className="link-no-decoration">
+          <span className="nav_logo_text">✅ TodoApp</span>
+        </Link>
+      </div>
+      <div className="nav_btns">
+        <NavBarThemChanger />
+        <NavBarContactOpener />
+        <NavBarAboutOpenBtn />
+      </div>
     </nav>
-}
+  );
+};
--- frontend/src/components/layouts/NavMenu/NavBarAbout/NavBarAboutOpenBtn/NavBarAboutOpenBtn.tsx
+++ frontend/src/components/layouts/NavMenu/NavBarAbout/NavBarAboutOpenBtn/NavBarAboutOpenBtn.tsx
@@ -1,30 +1,26 @@
-import React, {useState} from 'react';
-import './NavBarAboutOpenBtn.css'
-import {AboutContent} from "../AboutContent/AboutContent";
-
+import React, { useState } from "react";
+import "./NavBarAboutOpenBtn.css";
+import { AboutContent } from "../AboutContent/AboutContent";
 
 export const NavBarAboutOpenBtn = () => {
+  const [modal, setModal] = useState<boolean>(false);
 
-    const [modal, setModal] = useState<boolean>(false);
+  const toggleModalAboutContent = () => {
+    setModal(!modal);
+  };
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
 
-    const toggleModalAboutContent = () => {
-        setModal(!modal);
-    }
-    if (modal) {
-        document.body.classList.add('active-modal');
-    } else {
-        document.body.classList.remove('active-modal');
-    }
-
-    return (
-        <>
-            <button className="nav_btn-about" onClick={toggleModalAboutContent}>
-                <span>ABOUT</span>
-                <div className="fa fa-plus"></div>
-            </button>
-            {modal && <AboutContent onClick={toggleModalAboutContent}
-            />
-            }
-        </>
-    );
-}
+  return (
+    <>
+      <button className="nav_btn-about" onClick={toggleModalAboutContent}>
+        <span>ABOUT</span>
+        <div className="fa fa-plus"></div>
+      </button>
+      {modal && <AboutContent onClick={toggleModalAboutContent} />}
+    </>
+  );
+};
